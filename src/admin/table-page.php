59b46347b2fd647806bb842a686db12f1ca304e2
Pierre Saïkali <dev@mosaika.fr>
2018-11-19 09:18:17 +0100

    Adding some more documentation.

--- src/admin/table-page.php
+++ src/admin/table-page.php
@@ -16,7 +16,14 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Register the admin page and output its content.
  */
 class Table_Page implements Has_Hooks {
+	/**
+	 * User capability required to see the page.
+	 */
 	const CAPABILITY = 'list_users';
+
+	/**
+	 * The slug of our admin page.
+	 */
 	const SLUG = 'utec-list-table';
 
 	/**
@@ -26,14 +33,25 @@ class Table_Page implements Has_Hooks {
 		add_action( 'admin_menu', [ $this, 'add_admin_user_menu_subpage' ] );
 	}
 
+	/**
+	 * Add admin subpage
+	 */
 	public function add_admin_user_menu_subpage() {
 		add_submenu_page( 'users.php', __( 'Custom Codeable.io users table', 'utec' ), __( 'Custom table', 'utec' ), apply_filters( 'utec_admin_table_capability', self::CAPABILITY ), self::SLUG, [ $this, 'render_admin_page' ] );
 	}
 
+	/**
+	 * Render our admin subpage
+	 */
 	public function render_admin_page() {
 		Utils::get_template( 'admin-list-table.php', $this->prepare_data_for_table_page() );
 	}
 
+	/**
+	 * Prepare data for our table page
+	 *
+	 * @return array $data
+	 */
 	private function prepare_data_for_table_page() {
 		$request = Request_Utils::get_current_request();
 		$users   = Request_Utils::get_current_request_users();
@@ -54,6 +72,11 @@ class Table_Page implements Has_Hooks {
 		return $data;
 	}
 
+	/**
+	 * Get our admin page URL
+	 *
+	 * @return string The admin page URL.
+	 */
 	public static function get_table_page_admin_url() {
 		return add_query_arg( [ 'page' => self::SLUG ], admin_url( 'users.php' ) );
 	}
